1456637cbc212a58bed79f1152a5db36cfa34d5a
jasonpbenson <jasonbenson3rd@gmail.com>
2019-03-19 16:38:37 -0400

    v3–styles finished/modals retooled. Some content/copy needs to be updated

--- scripts.js
+++ scripts.js
@@ -19,18 +19,23 @@ $(".modalBackdrop").on('click', function(){
 
 let modalOn = false;
 
-$(".aboutButton").on('click', function(){
+$(".about").on('click', function(){
    modalOn = true;
    $(".modal").fadeIn("slow");
    $(".modalBackdrop").fadeIn("slow")
    modal.innerHTML = `
    <div class="aboutHeader">
-      <h2>Jason Benson</h2>
-      <p class="briefStatement">Full-stack web developer, artist, and designer 
-      based in Atlanta, GA</p>
-      <p><a href="./files/jason-benson-resume.pdf" target="_blank">Resumé</a></p>
+      <div class="imgContainer">
+         <img class="mugShot" src="./images/IMG_8992.JPG"/>
+      </div>
+      <h3>full-stack web developer, artist, and designer based in atlanta</h3>
+      <p class="briefStatement">I approach projects with a dual interest in design and programming, and 
+      work to generate ui that is playful, exploratory,  and idiosyncratic. 
+      My fine art background provides a basis for critical thinking and creative 
+      problem-solving that undergirds my lifelong interest in making things.</p>
    </div>
-   <div class="aboutTechContainer">
+   <div class="aboutTechContainer"> 
+      <h2>skills/experience</h2>
       <h3>front-end</h3>
       <div class="frontEnd">
          <div class="techIcon">
@@ -87,6 +92,10 @@ $(".aboutButton").on('click', function(){
             <i class="devicon-mysql-plain"></i>
             <p>mysql</p>
          </div>
+         <div class="techIcon">
+         <i class="devicon-postgresql-plain"></i>
+         <p>postgresql</p>
+      </div>
       </div>
       <h3>et al</h3>
       <div class="etAl">  
@@ -139,68 +148,61 @@ $(".aboutButton").on('click', function(){
    `
 })
 
-$(".projectsButton").on('click', function(){
+$(".projects").on('click', function(){
    modalOn = true;
    $(".modal").fadeIn("slow");
    $(".modalBackdrop").fadeIn("slow");
    modal.innerHTML =  `
-   <div class="pithosContainer">
-      <img class="pithosLogo" src="./images/pithos-logo-1.png">
-      <img class="pithosCap" src="./images/screencaps/pithos_screen1.png">
-      <a href="https://github.com/jasonpbenson/FrontEndProject-Pithos">repo</a>
-      <a href="https://jpbenson.com/pithos">demo</a>
-      <h3>project description</h3>
-      <p>Pithos is a space that allows the user to experience an 'ordinary' 
-      thing in an unusual way. We were interested in ecosystems, and wanted to connect our 
-      ecosystem to the state of Georgia (home), and also the larger realms of history, mythology, 
-      and health; which are their own kinds of ecosystems, or containers. Pithos.</p>
-      <h3>built with</h3>
-      <p>css3, html5, javascript, jquery</p>
-   </div>
+   <div class="projectsContainer">
+      <div class="elgContainer">
+         <img class="elgLogo" src="./images/enchanted_splatter_OffRegTrace_ed.png">
+         <div class="projectLinks">
+            <a href="https://github.com/jasonpbenson/enchantedLifeGoods">repo</a>
+            <a href="https://enchantedlifegoods.jpbenson.com">demo</a>
+         </div>
+         <img class="elgCap" src="./images/screencaps/elg_screen1.png">
+         <h3>project description</h3>
+         <p>Enchanted Life Goods is a digital marketplace for a small business that I 
+         manage with my wife; primarily we make and sell ceramics. The site is designed 
+         to be playful and intuitive, and is pretty straightforward in terms of functionality. 
+         The ‘marketplace’ is not currently live, but a demo is available via the link above. 
+         </p>
+         <h3>built with</h3>
+         <p>css3, jsx, nodejs/express, postgresql, react/redux</p>
+      </div>
       <div class="emOceansContainer">
-      <img class="emOceansLogo" src="./images/emOceans-logo-1.png">
-      <img class="emOceansCap" src="./images/screencaps/emOceans_screen2.png">
-      <a href="https://github.com/jasonpbenson/backEndProject">repo</a>
-      <a href="https://jpbenson.com/emoceans">demo</a>
-      <h3>project description</h3>
-      <p>
-      emOceans is a simple web application that allows you to take note of your mood 
-      every day using color and language. Each day (and each mood, each memory) forms 
-      a tiny drop in the vast and ever-shifting ocean that is every human. We hope to 
-      make the observation and appreciation of those shifts and waves more accessible, 
-      helpful, and more beautiful.
-      </p>
-      <h3>built with</h3>
-      <p>css3, express, html5, javascript, jquery, mysql, nodejs</p>
-   </div>
-   `
-})
-
-$(".contact").on('click', function(){
-   modalOn = true;
-   $(".modal").fadeIn("slow");
-   $(".modalBackdrop").fadeIn("slow")
-   modal.innerHTML = `
-   <div class="contactContainer">
-      <h2><a href="https://github.com/jasonpbenson">GitHub</a></h2>
-      <h2><a href="https://www.linkedin.com/in/jason-benson-960861180/">LinkedIn</a></h2>
-   </div>
-   <div class="contactGuy">
-      <img src="./images/contactGuy.png"/>
-   </div>
-   `
-})
-
-$(".title").on('click', function(){
-   modalOn = true;
-   $(".modal").fadeIn("slow");
-   $(".modalBackdrop").fadeIn("slow");
-   modal.innerHTML = `
-   <div class="gardenGuy">
-      <img src="./images/gardenGuy.png"/>
-   </div>
-   <div class="textScroll">
-      <p>But the sea which no one tends is also a garden </p>
+         <img class="emOceansLogo" src="./images/emOceans-logo-1.png">
+         <div class="projectLinks">
+            <a href="https://github.com/jasonpbenson/backEndProject">repo</a>
+            <a href="https://emoceans.jpbenson.com">demo</a>
+         </div>
+         <img class="emOceansCap" src="./images/screencaps/emOceans_screen2.png">
+         <h3>project description</h3>
+         <p>
+         emOceans is a simple web application that allows you to take note of your mood 
+         every day using color and language. Each day (and each mood, each memory) forms 
+         a tiny drop in the vast and ever-shifting ocean that is every human. We hope to 
+         make the observation and appreciation of those shifts and waves more accessible, 
+         helpful, and more beautiful.
+         </p>
+         <h3>built with</h3>
+         <p>css3, html5, javascript/jquery, mysql, nodejs/express</p>
+      </div>
+      <div class="pithosContainer">
+         <img class="pithosLogo" src="./images/pithos-logo-1.png">
+         <div class="projectLinks">
+            <a href="https://github.com/jasonpbenson/FrontEndProject-Pithos">repo</a>
+            <a href="https://pithos.jpbenson.com">demo</a>
+         </div>
+         <img class="pithosCap" src="./images/screencaps/pithos_screen1.png">
+         <h3>project description</h3>
+         <p>Pithos is a space that allows the user to experience an 'ordinary' 
+         thing in an unusual way. We were interested in ecosystems, and wanted to connect our 
+         ecosystem to the state of Georgia (home), and also the larger realms of history, mythology, 
+         and health; which are their own kinds of ecosystems, or containers. Pithos.</p>
+         <h3>built with</h3>
+         <p>css3, html5, javascript/jquery</p>
+      </div>
    </div>
    `
 })
